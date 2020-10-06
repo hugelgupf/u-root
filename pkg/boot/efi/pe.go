1fed691ec8296fdd21fe7c160c0abae98574260f
Chris Koch <chrisko@google.com>
2020-10-06 00:48:31 -0700

    efi: implement relocations for real
    Previous implementation assumed that page RVA == offset in EFI file,
    which was just so wrong.
    
    Signed-off-by: Chris Koch <chrisko@google.com>

--- pkg/boot/efi/pe.go
+++ pkg/boot/efi/pe.go
@@ -12,14 +12,43 @@ import (
 	"github.com/u-root/u-root/pkg/uio"
 )
 
-const IMAGE_SCN_MEM_DISCARDABLE = 0x02000000
+// sectionFlags describes the characteristics of each section.
+type sectionFlags uint32
 
-const M16 = 0x1000000
+// https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#section-flags
+var (
+	_IMAGE_SCN_MEM_DISCARDABLE sectionFlags = 0x02000000
+	_IMAGE_SCN_MEM_EXECUTE     sectionFlags = 0x20000000
+	_IMAGE_SCN_MEM_READ        sectionFlags = 0x40000000
+	_IMAGE_SCN_MEM_WRITE       sectionFlags = 0x80000000
+)
 
-type reloType uint8
+// machineType is the architecture target of the binary.
+type machineType uint16
 
+// https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#machine-types
+var (
+	// amd64/x86-64.
+	_IMAGE_FILE_MACHINE_AMD64 machineType = 0x8664
+
+	// ARM64 little-endian.
+	_IMAGE_FILE_MACHINE_ARM64 machineType = 0xaa64
+)
+
+const m16 = 0x1000000
+
+// relocType is a PE image relocation within a .reloc section.
+//
+// (To be distinguished from a COFF relocation.)
+type relocType uint8
+
+// https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#base-relocation-types
 const (
-	IMAGE_REL_BASED_DIR64 reloType = 10
+	// "The base relocation is skipped. This type can be used to pad a block."
+	_IMAGE_REL_BASED_ABSOLUTE relocType = 0
+
+	// "The base relocation applies the difference to the 64-bit field at offset."
+	_IMAGE_REL_BASED_DIR64 relocType = 10
 )
 
 // Load implements OSImage.Load.
@@ -33,7 +62,10 @@ func Segments(kernel io.ReaderAt) (kexec.Segments, uintptr, error) {
 		return nil, 0, err
 	}
 
-	log.Printf("kernelbuf: %d", len(kernelBuf))
+	if machineType(f.Machine) != _IMAGE_FILE_MACHINE_AMD64 {
+		return nil, 0, fmt.Errorf("EFI loader does not support arch %#x", f.Machine)
+	}
+	log.Printf("file characteristics: %#x", f.Characteristics)
 
 	var (
 		entry     uintptr
@@ -45,21 +77,34 @@ func Segments(kernel io.ReaderAt) (kexec.Segments, uintptr, error) {
 
 	case *pe.OptionalHeader64:
 		entry = uintptr(oh.AddressOfEntryPoint)
+		imageBase = oh.ImageBase
+
 		log.Printf("base of code: %#x", oh.BaseOfCode)
 		log.Printf("image base: %#x", oh.ImageBase)
-		imageBase = oh.ImageBase
 	}
 
-	//chosenBase := uintptr(0x100000000)
 	var chosenBase uintptr
 	if imageBase == 0 {
+		// In qemu, on Linux, this physical address usually _HAPPENS_
+		// to be free.
+		//
+		// TODO: move EFI kexec to a /dev, add ioctl for asking for an
+		// allocation.
 		chosenBase = 0x10000000
 	} else {
-		chosenBase = uintptr(imageBase)
+		// Add 0x1000 just to make sure relocations are working.
+		chosenBase = uintptr(imageBase) + 0x1000
 	}
 
 	log.Printf("entry: %#x", entry)
 
+	// A note on terminology: the PE image format calls the target
+	// address of each section the "virtual address" and the length the
+	// "virtual size". Relocations refer to "page RVA" (relative virtual
+	// address).
+	//
+	// In kexec, virtual target address == physical target address, so we
+	// refer to the target address as the physical address as well.
 	var segment kexec.Segments
 
 	// Windows loader expects to also fing the header of the EFI file.
@@ -78,10 +123,15 @@ func Segments(kernel io.ReaderAt) (kexec.Segments, uintptr, error) {
 	segment = append(segment, s)
 
 	var reloc *pe.Section
-	// Now add the actuall sections
+
+	// Now add the actual sections.
 	for _, section := range f.Sections {
 		if section.Name == ".reloc" {
 			reloc = section
+		}
+		// "The section can be discarded as needed." (usually .reloc)
+		if sectionFlags(section.Characteristics)&_IMAGE_SCN_MEM_DISCARDABLE != 0 {
+			continue
 		}
 		s := kexec.Segment{
 			Buf: kexec.Range{
@@ -94,12 +144,18 @@ func Segments(kernel io.ReaderAt) (kexec.Segments, uintptr, error) {
 			},
 		}
 		segment = append(segment, s)
-	}
-	for _, s := range segment {
-		log.Printf("%s", s)
-		//log.Printf("virt: %#x + %#x | phys: %#x + %#x", s.Buf.Start, s.Buf.Size, s.Phys.Start, s.Phys.Size)
+
+		log.Printf("%s (%s - virtual range %s)", s, section.Name, kexec.Range{Start: uintptr(section.VirtualAddress), Size: uint(section.VirtualSize)})
 	}
 
+	// Note that there is a difference between COFF relocations and PE
+	// image relocations.
+	//
+	// COFF relocations are per-section and in the parsed section.Relocs in
+	// the debug/pe Go API.
+	//
+	// PE image relocations are in a section named ".reloc" and must be
+	// manually parsed.
 	if reloc != nil {
 		log.Printf("reloc: %s", reloc)
 
@@ -119,7 +175,7 @@ func Segments(kernel io.ReaderAt) (kexec.Segments, uintptr, error) {
 				break
 			}
 
-			relocs := make([]uint16, (hdr.TotalSize-8)/2)
+			relocs := make([]uint16, (hdr.TotalSize-uint32(unsafe.Sizeof(hdr)))/2)
 			if err := binary.Read(r, binary.LittleEndian, relocs); err == io.EOF {
 				return nil, 0, fmt.Errorf("wrong number of elements %d for total size %d", len(relocs), hdr.TotalSize)
 			} else if err != nil {
@@ -129,24 +185,31 @@ func Segments(kernel io.ReaderAt) (kexec.Segments, uintptr, error) {
 			diff := int64(chosenBase) - int64(imageBase)
 
 			for _, relo := range relocs {
-				// 4 bits relocation type. 12 bits offset.
-				reloT := reloType(relo & 0xf000 >> 12)
+				// Every relocation is 16 bits.
+				//
+				// 4 bits relocation type
+				// 12 bits offset
+				typ := relocType(relo & 0xf000 >> 12)
 				offset := relo & 0xfff
 
-				switch reloT {
-				case IMAGE_REL_BASED_DIR64:
+				switch typ {
+				case _IMAGE_REL_BASED_DIR64:
 					imageOffset := hdr.PageRVA + uint32(offset)
 
-					value := int64(binary.LittleEndian.Uint64(kernelBuf[imageOffset:]))
-					value += diff
+					buf := segment.GetPhys(kexec.Range{Start: chosenBase + uintptr(imageOffset), Size: 8})
+					if buf == nil || len(buf) < 8 {
+						return nil, 0, fmt.Errorf("relocation at address %#x could not be found in image", imageOffset)
+					}
 
-					binary.LittleEndian.PutUint64(kernelBuf[imageOffset:], uint64(value))
+					value := int64(binary.LittleEndian.Uint64(buf))
+					binary.LittleEndian.PutUint64(buf, uint64(value+diff))
 
-				case 0:
+				case _IMAGE_REL_BASED_ABSOLUTE:
+					// "relocation is skipped."
 					continue
 
 				default:
-					return nil, 0, fmt.Errorf("relocation of type % not implemented", reloT)
+					return nil, 0, fmt.Errorf("relocation of type %d not implemented", typ)
 				}
 			}
 		}
